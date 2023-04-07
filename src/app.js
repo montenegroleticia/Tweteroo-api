e16c4d3462140ff7f57861ba196be0cbfa8cca05
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-04-07 15:59:29 -0300

    tweets empty

--- src/app.js
+++ src/app.js
@@ -13,12 +13,7 @@ const profile = [
       "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
   },
 ];
-const tweets = [
-  {
-    username: "bobesponja",
-    tweet: "Eu amo hambúrguer de siri!",
-  },
-];
+const tweets = [];
 
 app.post("/sign-up", (request, response) => {
   const { username, avatar } = request.body;
