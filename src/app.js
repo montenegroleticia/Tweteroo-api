455bff2d4fec9c5fafbbbca91b2e871602400aba
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-04-07 13:59:02 -0300

    Made the posts and get

---
+++ src/app.js
@@ -0,0 +1,69 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const user = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+  },
+];
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+];
+
+app.post("/sign-up", (request, response) => {
+  const { username, avatar } = request.body;
+
+  if (!username || !avatar) {
+    return response.status(422).send("Todos os campos são obrigatórios!");
+  }
+
+  const newUser = { username, avatar };
+
+  user.push(newUser);
+  response.status(201).send("OK");
+});
+
+app.post("/tweets", (request, response) => {
+  const { username, tweet } = request.body;
+
+  const findUsername = user.find((name) => name.username === username);
+
+  if (!findUsername) {
+    return response.status(401).send("UNAUTHORIZED");
+  }
+
+  if (!username || !tweet) {
+    return response.status(422).send("Todos os campos são obrigatórios");
+  }
+
+  const newTweet = { username, tweet };
+
+  tweets.push(newTweet);
+  response.status(201).send("OK");
+});
+
+app.get("/tweets", (request, response) => {
+  const tweetsWithAvatars = tweets.map((t) => {
+    const { username, tweet } = t;
+
+    const findAvatar = user.find((a) => a.username === t.username);
+    const avatar = findAvatar ? findAvatar.avatar : null;
+
+    return { username, avatar, tweet };
+  });
+
+  response.send(tweetsWithAvatars);
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
